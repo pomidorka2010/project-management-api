a2a9cda70e7b8766fa7a503c67240f1cc3129b74
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-01 17:38:21 +0200

    feat: initial commit for routes/projects.js

---
+++ routes/projects.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Project = require('../models/Project');
+
+// Create a new project
+router.post('/', async (req, res) => {
+  try {
+    const newProject = new Project(req.body);
+    const savedProject = await newProject.save();
+    res.status(201).json(savedProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Retrieve all projects
+router.get('/', async (req, res) => {
+  try {
+    const projects = await Project.find();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
