ef09ebc31ff118bbc3b3076e03e3eb1c9d5c6bc5
pomidorka2010 <lamc560ki@hotmail.com>
2025-09-01 17:38:19 +0200

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const projectRoutes = require('./routes/projects');
+const taskRoutes = require('./routes/tasks');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+// Database connection
+mongoose.connect('mongodb://localhost/project_management', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.use('/api/projects', projectRoutes);
+app.use('/api/projects/:projectId/tasks', taskRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
